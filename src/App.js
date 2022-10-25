7c90be09ff522359c3d03a06be52d01eb961e294
gokri20 <gokri20@student.sdu.dk>
2022-10-25 17:10:49 +0200

    Used router-dom in the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Suspense, lazy} from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Frontpage from "./Frontpage"
+
+const Page2 = lazy(() => import("./Page2"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Morning, nice day for fishing ain't it?</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Suspense fallback="I am loading">
+            <Link to="/">Home</Link> <space/>
+            <Link to="page2">Page 2</Link>
+            <Routes>
+              <Route index element={<Frontpage />}/>
+              <Route path="/page2" element={<Page2 />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>        
     </div>
   );
 }
---
+++ src/Frontpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h2>Welcome to Frontpage</h2>
+            <p>Morning, nice day for fishing ain't it?</p>
+        </div>
+    );
+}
+    
+export default App;
