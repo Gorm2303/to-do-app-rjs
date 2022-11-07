91dc1aca28ab72021eeb69dc12cb7b39ddfc77d9
gokri20 <gokri20@student.sdu.dk>
2022-11-07 14:23:51 +0100

    todo list works

--- src/List.js
+++ src/List.js
@@ -1,15 +1,38 @@
 import Alert from 'react-bootstrap/Alert';
 import React, { useState} from "react";
+import TextField from '@mui/material/TextField'
+import Button from 'react-bootstrap/Button'; 
+import './styles.scss'; 
 
 function App() {
-    const [list, setList] = useState([1,2])
+    const [list, setList] = useState(['Homework', 'Dinner', 'Fitness']);
+    const [inputText, setInputText] = useState('');
+
+    function addItem() {
+        setList([...list, inputText]);
+        setInputText('');
+    }
+
+    function handleChange(event) {
+        setInputText(event.target.value);
+    }
+
+    function removeItem(item) {
+        const newList = list.filter((i) => i !== item);
+
+        setList(newList);
+    }
 
     const alerts = list.map((item) => {
-        return <Alert dismissible onClose={true} severity="info">{item}</Alert>
+        return <Alert className="alert" dismissible onClose={() => removeItem(item)} severity="info">{item}</Alert>
     })
 
     return (
         <div>
+            <Alert severity="info">
+            <TextField id="standard-basic" value={inputText} label="To Do" variant="standard" onChange={handleChange}/>
+            <Button variant="primary" onClick={() => addItem()}>Add</Button>{''}
+            </Alert>
             {alerts}
         </div>
     );
